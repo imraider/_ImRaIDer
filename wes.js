e1871bcb957935bb6e35928969008b492f83451d
imraider <45240051+imraider@users.noreply.github.com>
2018-11-23 19:42:53 +0200

    Update wes.js

--- wes.js
+++ wes.js
@@ -50,4 +50,4 @@ if (message.content.startsWith(adminprefix + 'st')) {
 
 });
 
-client.login('process.env.BOT_TOKEN');
+client.login('mfa.Jskkmsri2wwBKZASQEAeN1KAmWVf8ofaHxwmrNFsv0R6HEWcpPBCOe6W7cPxsHSzdZ6PKqOF_uWK9evqi-hE');
